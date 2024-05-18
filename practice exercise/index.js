ab02b0cf93888db9e880d289b55ff9934a5c53bd
piyush4596 <piyushprajapati3008@gmail.com>
2024-05-17 23:01:52 -0400

    practice

---
+++ practice exercise/index.js
@@ -0,0 +1,21 @@
+// 1. write a JavaScript program to check two numbers and return true if one of the number is 100
+// or if the sum of the two numbers is 100
+
+// const isEqualTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
+// const isEqualTo100 = (a, b) => {
+//   return a === 100 || b === 100 || a + b === 100;
+// };
+
+// console.log(isEqualTo100(100, 0));
+// console.log(isEqualTo100(0, 100));
+// console.log(isEqualTo100(60, 40));
+// console.log(isEqualTo100(50, 70));
+// console.log(isEqualTo100(10, 20));
+
+// 2. write a javascript program to get the extension of a file.
+
+// getExtension = function (str) {
+//   return str.slice(str.lastIndexOf("."));
+// };
+
+// console.log(getExtension("piyush.jkl"));
