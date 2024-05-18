ad4e0605ba4157fc2c1b33419bd26c7778fa8277
piyush4596 <piyushprajapati3008@gmail.com>
2024-05-17 23:53:09 -0400

    more exercise

--- practice exercise/index.js
+++ practice exercise/index.js
@@ -11,7 +11,7 @@
 // console.log(isEqualTo100(60, 40));
 // console.log(isEqualTo100(50, 70));
 // console.log(isEqualTo100(10, 20));
-
+///////////////////////////////////////////////////////////////////////////
 // 2. write a javascript program to get the extension of a file.
 
 // getExtension = function (str) {
@@ -19,3 +19,17 @@
 // };
 
 // console.log(getExtension("piyush.jkl"));
+////////////////////////////////////////////////////////////////////////
+//3. write a javascript program to replace every character in a given string with the character following it in the alphabet
+
+// The fromCharCode and charCodeAt methods in JavaScript are used for converting between characters and their corresponding Unicode values.
+// const moveForwardChar = (str) =>
+//   str
+//     .split("")
+//     .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+//     .join("");
+
+// console.log(moveForwardChar("abcdef123"));
+/////////////////////////////////////////////////////////////////////////
+
+//4. write program to get the current date
