3e69c4d6bc3e20c226125e88bbc4c85da91ee7b7
piyush4596 <piyushprajapati3008@gmail.com>
2024-05-23 00:00:08 -0400

    .

--- mini projects/Snake game/app.js
+++ mini projects/Snake game/app.js
@@ -1,2 +1,6 @@
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContex("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
