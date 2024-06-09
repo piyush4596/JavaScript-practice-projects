7d32257c11e39bfefbc03d95b5e045b9d162feb0
piyush4596 <piyushprajapati3008@gmail.com>
2024-06-09 00:53:21 -0400

    instagram ejs temp

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -20,3 +20,8 @@ app.get("/rolldice", (req, res) => {
   let diceVal = Math.floor(Math.random() * 6) + 1;
   res.render("rolldice.ejs", { diceVal });
 });
+
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  res.render("instagram.ejs", { username });
+});
